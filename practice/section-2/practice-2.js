f06d316d912972dccb5ab9282f16c5d8a723b7dc
Wuuboy <30542499+Wuuboy@users.noreply.github.com>
2019-07-08 22:10:56 +0800

    Update practice-2.js

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,34 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array()
+  var dict = {}
+  for(var i = 0;i < collection.length; i++){
+    if(collection[i].indexOf("-")==-1){
+      if(collection[i] in dict){
+        dict[collection[i]]++
+      }else {
+        dict[collection[i]] = 1
+      }
+    }else {
+      var temp_key = collection[i].split("-")[0]
+      var temp_value = collection[i].split("-")[1]
+      if(temp_key in dict){
+        dict[temp_key]++
+      }else {
+        dict[temp_key] = Number(temp_value)
+                }
+    }
+
+  }
+  //处理输出格式
+  for ( var key in dict){
+    var dict_form = {
+      key : key,
+      count : dict[key]
+    }
+    result.push(dict_form)
+  }
+  console.log(result)
+  return result
 }
